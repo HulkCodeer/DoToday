e906d0eb7832a81b3c0b56d036698810144c9f1f
hyunjin.park <unicon85@gmail.com>
2020-03-16 23:06:12 +0900

    :construction: POD Setting

---
+++ DoToday/Common/View/CommonViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CommonViewController.swift
+//  DoToday
+//
+//  Created by 박현진 on 2020/03/15.
+//  Copyright © 2020 박현진. All rights reserved.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+internal class CommonViewController: UIViewController {
+    
+}
